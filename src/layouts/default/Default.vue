dadabf89277522fe707598a143bd25b144aff420
Ibrahim Sule <ibraheemsulay@gmail.com>
2023-08-28 06:41:13 +0100

    created the boilerplate for the project

---
+++ src/layouts/default/Default.vue
@@ -0,0 +1,12 @@
+<template>
+  <VApp>
+    <default-bar />
+
+    <default-view />
+  </VApp>
+</template>
+
+<script lang="ts" setup>
+import DefaultBar from "./AppBar.vue";
+import DefaultView from "./View.vue";
+</script>
